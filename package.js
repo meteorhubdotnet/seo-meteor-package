e8f3054b615d110b72aad39c7f6415cf959e8535
Yacine Merzouk <yacine@merzouk.ca>
2019-08-02 19:21:11 -1000

    Updated package dependencies

--- package.js
+++ package.js
@@ -3,13 +3,13 @@ var path = Npm.require('path');
 
 Package.describe({
   "summary": "Render SEO data for all bots and users",
-  "version": "1.0.0",
+  "version": "1.0.1",
   "git": "https://github.com/meteorhubdotnet/seo-meteor-package",
   "name": "meteorhubdotnet:seo"
 });
 
 Npm.depends({
-  "connect": "2.13.0"
+  "connect": "3.7.0"
 });
 
 Package.onUse(function(api) {
@@ -18,9 +18,9 @@ Package.onUse(function(api) {
 });
 
 function configure (api) {
-  api.versionsFrom('METEOR@0.9.3');
-  api.use('meteorhacks:inject-initial@1.0.2', ['client', 'server']);
-  api.use('iron:router@0.9.0 || 1.0.0', ['client', 'server'], {weak: true});
+  api.versionsFrom('1.8.1');
+  api.use('meteorhacks:inject-initial@1.0.4', ['client', 'server']);
+  api.use('iron:router@1.1.2', ['client', 'server'], {weak: true});
 
   api.use('meteorhacks:picker@1.0.3', 'server');
 
